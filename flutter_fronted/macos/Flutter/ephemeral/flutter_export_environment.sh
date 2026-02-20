059d6dbc8199de9c0f192d42936e7efe923b8873
Synonimity <252955592+synontechsa-hub@users.noreply.github.com>
2026-02-20 13:46:45 +0200

    chore: Add generated Flutter platform configuration and plugin registration files for iOS, macOS, and Android.

---
+++ flutter_fronted/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\brcgr\.develop\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\SynonTech\SoulLink\SoulLink_v1.5.0\flutter_fronted"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.5.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
